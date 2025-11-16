0b28272d13113e3b3fcdcd7b22a80e7171c991d2
yelizaveta-pystina <yelizavetapystina@gmail.com>
2025-11-15 23:35:59 -0800

    seperate Player and NPC Scene + SceneLoader via MainScene

---
+++ Assets/DenysAlmaral/CityPeople-FREE/Scripts/SceneLoader.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [Header("Scenes to Load")]
+    public string[] scenesToLoad = new string[]
+    {
+        "PlayerScene",
+        "NPCScene"
+    };
+    
+    void Awake()
+    {
+        Debug.Log("SceneLoader: Starting to load scenes...");
+        
+        foreach (string sceneName in scenesToLoad)
+        {
+            Debug.Log("SceneLoader: Attempting to load " + sceneName);
+            
+            if (!SceneManager.GetSceneByName(sceneName).isLoaded)
+            {
+                SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+                Debug.Log("SceneLoader: Loaded " + sceneName);
+            }
+            else
+            {
+                Debug.Log("SceneLoader: " + sceneName + " already loaded");
+            }
+        }
+    }
+}
